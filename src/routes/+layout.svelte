6d6e44224e6dcabe7952d7d61209d71a77644785
split <split@split.pet>
2024-06-24 17:00:52 -0700

    use bun, install inter

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import "@fontsource-variable/inter"
+</script>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>ava</title>
+        <style>
+            body {
+                font-family: "Inter Variable", "Inter", sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <slot />
+    </body>
+</html>
