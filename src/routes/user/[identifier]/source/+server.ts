17814b3478a1196129a8c7265718e0ef47d5b3bc
split <split@split.pet>
2024-11-20 07:40:49 -0800

    fix: :bug: redirecting to 404

--- src/routes/user/[identifier]/source/+server.ts
+++ src/routes/user/[identifier]/source/+server.ts
@@ -1,10 +1,10 @@
-import { getMetadataForIdentifier } from '$lib/avatars.js';
-import { error, redirect } from '@sveltejs/kit';
+import { getMetadataForIdentifier } from "$lib/avatars.js"
+import { error, redirect } from "@sveltejs/kit"
 
-export async function GET({ params : { identifier } }) {
+export async function GET({ params: { identifier } }) {
     let { source } = await getMetadataForIdentifier(identifier)
-    let finalUrl: URL | string = `/info/${identifier}`
-    
+    let finalUrl: URL | string = `/user/${identifier}`
+
     if (source) {
         try {
             finalUrl = new URL(source)
