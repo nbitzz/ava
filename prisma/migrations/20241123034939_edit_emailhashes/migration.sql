b2b1e564627d6c691418da89907d3047d6f54418
split <split@split.pet>
2024-11-22 19:50:51 -0800

    fix: :ambulance: im not gonna bother fixing this fuck you

--- prisma/migrations/20241123034939_edit_emailhashes/migration.sql
+++ prisma/migrations/20241123034939_edit_emailhashes/migration.sql
@@ -1,6 +1,8 @@
 /*
   Warnings:
 
+  - The primary key for the `EmailHashes` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The required column `email` was added to the `EmailHashes` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
   - The required column `id` was added to the `EmailHashes` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
 
 */
@@ -10,14 +12,14 @@ PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_EmailHashes" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "forUserId" TEXT NOT NULL,
-    "email" TEXT NOT NULL DEFAULT 'unknown:${uuid()}',
+    "email" TEXT NOT NULL,
     "sha256" BLOB NOT NULL,
     "md5" BLOB NOT NULL,
     "isPrimaryForUserId" TEXT,
     CONSTRAINT "EmailHashes_forUserId_fkey" FOREIGN KEY ("forUserId") REFERENCES "User" ("userId") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "EmailHashes_isPrimaryForUserId_fkey" FOREIGN KEY ("isPrimaryForUserId") REFERENCES "User" ("userId") ON DELETE SET NULL ON UPDATE CASCADE
 );
-INSERT INTO "new_EmailHashes" ("email", "forUserId", "isPrimaryForUserId", "md5", "sha256") SELECT "email", "forUserId", "isPrimaryForUserId", "md5", "sha256" FROM "EmailHashes";
+INSERT INTO "new_EmailHashes" ("forUserId", "md5", "sha256") SELECT "forUserId", "md5", "sha256" FROM "EmailHashes";
 DROP TABLE "EmailHashes";
 ALTER TABLE "new_EmailHashes" RENAME TO "EmailHashes";
 CREATE UNIQUE INDEX "EmailHashes_email_key" ON "EmailHashes"("email");
